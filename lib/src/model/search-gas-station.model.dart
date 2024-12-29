192e4e9c25b351286243e27e4a31f799f7237a56
Clément <23141589+clementherve@users.noreply.github.com>
2024-12-29 17:29:46 +0100

    add serialization to search gas station model

--- lib/src/model/search-gas-station.model.dart
+++ lib/src/model/search-gas-station.model.dart
@@ -16,4 +16,24 @@ class SearchGasStation {
     this.constrainingIds = const [],
     this.lastUpdatedDays = const Duration(days: 1),
   });
+
+  fromJson(Map<String, dynamic> json) {
+    location = LatLng(json['location']['lat'], json['location']['lng']);
+    searchRadiusMeters = json['search_radius_meters'];
+    fuelType = json['fuel_type'];
+    alwaysOpen = json['always_open'];
+    constrainingIds = json['constraining_ids'];
+    lastUpdatedDays = Duration(days: json['last_updated_days']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'location': {'lat': location.latitude, 'lng': location.longitude},
+      'search_radius_meters': searchRadiusMeters,
+      'fuel_type': fuelType,
+      'always_open': alwaysOpen,
+      'constraining_ids': constrainingIds,
+      'last_updated_days': lastUpdatedDays.inDays,
+    };
+  }
 }
